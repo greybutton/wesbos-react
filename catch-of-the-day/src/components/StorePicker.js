8e0fbb8b05313532539ac9c5fd1e33a9fb0b6a53
greybutton <greybutton@yandex.com>
2018-07-04 13:06:54 +0800

    step 4 (Writing HTML with JSX)

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
-export default () => <p>hey</p>;
+export default () => (
+  <form className="store-selector">
+    <h2>Please enter a store</h2>
+    <input type="text" required placeholder="Store name" />
+    <button type="submit">Visit store</button>
+  </form>
+);
