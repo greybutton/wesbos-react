283c08dd14b5597c01c92510e34be2cb0cd9121e
greybutton <greybutton@yandex.com>
2018-07-05 17:45:20 +0800

    step 6 (Creating our application layout with components)

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      day
+    </h1>
+    <h3 className="tagline">
+      <span>Fresh daily</span>
+    </h3>
+  </header>
+);
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <div className="order">order</div>;
