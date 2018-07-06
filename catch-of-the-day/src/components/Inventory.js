df5f0ad2098a7a5b726b763ebd226566f7b8f598
greybutton <greybutton@yandex.com>
2018-07-06 15:25:03 +0800

    step 13 (Understanding State)

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,3 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import AddFishForm from "./AddFishForm";
 
-export default () => <div className="inventory">inventory</div>;
+class Inventory extends Component {
+  render() {
+    return (
+      <div className="inventory">
+        <h2>inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
+  }
+}
+
+export default Inventory;
