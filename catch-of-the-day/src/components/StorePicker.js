a013a827400594df571c95da29ca451a80fe6264
greybutton <greybutton@yandex.com>
2018-07-06 14:30:31 +0800

    step 11, 12 (Handling Events)

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
-export default () => (
-  <form className="store-selector">
-    <h2>Please enter a store</h2>
-    <input type="text" required placeholder="Store name" />
-    <button type="submit">Visit store</button>
-  </form>
-);
+class StorePicker extends Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please enter a store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit store</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
