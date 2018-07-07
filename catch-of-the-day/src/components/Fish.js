184bd3031f6ad87c6d7da16f11ffeaee70fe70d7
greybutton <greybutton@yandex.com>
2018-07-07 21:54:30 +0800

    step 16 (Updating our Order State)

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { formatPrice } from "../helpers";
 
-const Fish = props => (
-  <li className="menu-fish">
-    <img src={props.details.image} alt={props.details.name} />
-    <h3 className="fish-name">
-      {props.details.name}
-      <span className="price">{formatPrice(props.details.price)}</span>
-    </h3>
-    <p>{props.details.desc}</p>
-    <button>Add to cart</button>
-  </li>
-);
+const Fish = props => {
+  const isAvailable = props.details.status === "available";
+  const handleClick = () => {
+    props.addToOrder(props.index);
+  };
+  return (
+    <li className="menu-fish">
+      <img src={props.details.image} alt={props.details.name} />
+      <h3 className="fish-name">
+        {props.details.name}
+        <span className="price">{formatPrice(props.details.price)}</span>
+      </h3>
+      <p>{props.details.desc}</p>
+      <button disabled={!isAvailable} onClick={handleClick}>
+        {isAvailable ? "Add to order" : "Sold out!"}
+      </button>
+    </li>
+  );
+};
 
 export default Fish;
