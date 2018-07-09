4b69516a9fa2d8be250efcb62ef0a767b1a48fe4
greybutton <greybutton@yandex.com>
2018-07-09 14:04:49 +0800

    step 22 (Animating React Components)

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { formatPrice } from "../helpers";
 
 const Order = props => {
@@ -16,21 +17,39 @@ const Order = props => {
   return (
     <div className="order-wrap">
       <h2>Order</h2>
-      <ul className="order">
+      <TransitionGroup component="ul" className="order">
         {orderIds.map(key => {
           const fish = props.fishes[key];
           const count = props.order[key];
           const isAvailable = fish && fish.status === "available";
+          const transitionOptions = {
+            classNames: "order",
+            key,
+            timeout: { enter: 500, exit: 500 }
+          };
           if (!fish) return null;
           if (isAvailable) {
             return (
-              <li key={key}>
-                {count} lbs {fish.name}
-                {formatPrice(count * fish.price)}
-                <button onClick={() => props.removeFromOrder(key)}>
-                  &times;
-                </button>
-              </li>
+              <CSSTransition {...transitionOptions}>
+                <li key={key}>
+                  <span>
+                    <TransitionGroup component="span" className="count">
+                      <CSSTransition
+                        classNames="count"
+                        key={count}
+                        timeout={{ enter: 500, exit: 500 }}
+                      >
+                        <span>{count}</span>
+                      </CSSTransition>
+                    </TransitionGroup>
+                    lbs {fish.name}
+                    {formatPrice(count * fish.price)}
+                    <button onClick={() => props.removeFromOrder(key)}>
+                      &times;
+                    </button>
+                  </span>
+                </li>
+              </CSSTransition>
             );
           }
           return (
@@ -39,7 +58,7 @@ const Order = props => {
             </li>
           );
         })}
-      </ul>
+      </TransitionGroup>
       <div className="total">
         Total: <strong>{formatPrice(total)}</strong>
       </div>
